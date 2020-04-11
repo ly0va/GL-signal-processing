b16f7eaa46b097f42559345ddd29097c83cc5c35
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2020-04-11 18:18:20 +0300

    Added usage and altered point type

--- graph.py
+++ graph.py
@@ -4,11 +4,16 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+if len(sys.argv) < 3:
+    print("USAGE: ./graph.py FILENAME SAMPLE_RATE")
+    exit(0)
+
 filename = sys.argv[1]
 fs = float(sys.argv[2])
 
-y = [int(x) for x in open(filename).readlines()]
-x = np.arange(0, (len(y)-1)/fs, 1/fs)
+y = [float(x) for x in open(filename).readlines()]
+x = np.arange(0, len(y)/fs, 1/fs)
+y = y[:len(x)]
 
 plt.plot(x, y)
 plt.xlabel('x')
